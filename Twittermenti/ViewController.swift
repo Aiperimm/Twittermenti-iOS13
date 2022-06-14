54e4221cc01a5d2788318c454a1de09b0b5ff383
AIPERIM AKISHEVA <AIPERIM1995@GMAIL.COM>
2022-06-13 20:41:58 -0400

    adding constant prediction with sentimentClassifier using CoreML

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -8,20 +8,27 @@
 
 import UIKit
 import SwifteriOS
+import CoreML
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let sentimentClassifier = TweetSentimentClassifier()
 
   let swifter = Swifter(consumerKey: "9F4Rnej4pW39Dj1FvnFSsneQW", consumerSecret: "tInTcFmb1AXiLEZyyxjpRfvnzbVqqFNwB1Zu4YFFJJ6hZGf6Qb")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let prediction = try! sentimentClassifier.prediction(text:"@Apple is the best company!")
+        
+        print(prediction.label)
+        
         swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { results, searchMetadata in
-            print(results)
+            //print(results)
             
         }) { error in
             print("There was an error with the Twitter API Request, \(error)")
