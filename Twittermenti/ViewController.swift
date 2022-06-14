8b898512758f4a5c1d9a7484a23496204b5fab44
AIPERIM AKISHEVA <AIPERIM1995@GMAIL.COM>
2022-06-13 22:09:15 -0400

    ADDED LABELS WITH EMODJI

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -24,49 +24,64 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { results, searchMetadata in
-            
-            var tweets = [TweetSentimentClassifierInput]()
-            
-            for i in 0..<100 {
-                if let tweet = results[i]["full_text"].string {
-                    let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
-                    tweets.append(tweetForClassification)
-                    
-                }
-            
-            }
-            do {
-                
-              let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
-                
-                var sentimentScore = 0
-                
-                for pred in predictions {
-                    let sentiment = pred.label
-                    
-                    if sentiment == "Pos" {
-                        sentimentScore += 1
-                    } else if sentiment == "Neg" {
-                        sentimentScore -= 1
-                    }
-                }
-                
-                print(sentimentScore)
-                
-                
-            }catch{
-                print("There was an error with making a prediction, \(error)")
-            }
-            
-            
-        }) { error in
-            print("There was an error with the Twitter API Request, \(error)")
-        }
-        
     }
 
     @IBAction func predictPressed(_ sender: Any) {
+        
+        if let searchText = textField.text {
+            swifter.searchTweet(using: searchText, lang: "en", count: 100, tweetMode: .extended, success: { results, searchMetadata in
+                
+                var tweets = [TweetSentimentClassifierInput]()
+                
+                for i in 0..<100 {
+                    if let tweet = results[i]["full_text"].string {
+                        let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
+                        tweets.append(tweetForClassification)
+                        
+                    }
+                
+                }
+                do {
+                    
+                  let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+                    
+                    var sentimentScore = 0
+                    
+                    for pred in predictions {
+                        let sentiment = pred.label
+                        
+                        if sentiment == "Pos" {
+                            sentimentScore += 1
+                        } else if sentiment == "Neg" {
+                            sentimentScore -= 1
+                        }
+                    }
+                    
+                    if sentimentScore > 20 {
+                        self.sentimentLabel.text = "😇"
+                    } else if sentimentScore > 10 {
+                        self.sentimentLabel.text = "😀"
+                    }else if sentimentScore > 0 {
+                        self.sentimentLabel.text = "🙂"
+                    }else if sentimentScore == 0 {
+                        self.sentimentLabel.text = "😒"
+                    }else if sentimentScore > -20 {
+                        self.sentimentLabel.text = "🤬"
+                    }else{
+                        self.sentimentLabel.text = "🤢"
+                    }
+                    
+                }catch{
+                    print("There was an error with making a prediction, \(error)")
+                }
+                
+                
+            }) { error in
+                print("There was an error with the Twitter API Request, \(error)")
+            }
+            
+            
+        }
     
     
     }
